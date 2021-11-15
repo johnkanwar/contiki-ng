26e65f7975678bbf5ce1b4045c83bd159eda5312
megman <johnkanwar@hotmail.com>
2021-11-15 12:47:12 +0100

    Add prints for different memory locations

--- examples/trust_zone/non_secure_zone/example_ns.c
+++ examples/trust_zone/non_secure_zone/example_ns.c
@@ -1,84 +1,47 @@
 #if (__ARM_FEATURE_CMSE & 1) == 0
 #error "Need ARMv8-M security extensions"
 #elif (__ARM_FEATURE_CMSE & 2) != 0
-#error "Don't compile with --cmse in order to make the build non-secure"
+#error "Don't compile with --cmse "in order to make the build" non-secure"
 #endif
 
-
 #include "contiki.h"
-
 #include <stdio.h> /* For printf() */
 
-/*nRF includes*/
-#include "hal/nrf_gpio.h"
-#include "hal/nrf_reset.h"
-#include "hal/nrf_spu.h"
-#include "dev/leds.h"
-#include "dev/button-hal.h"
-#include "nrf5340_application.h" //Includes core_cm33
-#include "system_nrf5340_application.h"
-#include "tz_context.h"
-//#include <arm_cmse.h>     // CMSE definitions
-//#include "SPM_lite.h"
-//#include "example_ns.h"
-#include "veneer_table.h"
-
-
-///*---------------------------------------------------------------------------*/
-//void
-//secure_init(void)
-//{
-//PROCESS_NAME(example_ns_process);
-//  process_start(&example_ns_process, NULL);
-//}
-///*---------------------------------------------------------------------------*/
-//int
-//get_int_nr(int val){
-//    return val;
-//}
+static int non_secure_variable = 80;
+const static int non_secure_variable1 = 81;
+static int non_secure_variable3;
+int non_secure_variable4;
+non_secure_variable4 = 84;
 
 /*---------------------------------------------------------------------------*/
 PROCESS(example_ns_process, "example ns process");
-AUTOSTART_PROCESSES(&example_ns_process);
+AUTOSTART_PROCESSES(&example_ns_process); //&example_ns_process
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(example_ns_process, ev, data)
 {
+  non_secure_variable3 = 83;
 
-  static struct etimer timer;
-  static int i = 0;
-  //static int id_nr = 32;
-  //static tz_nonsecure_setup_conf_t nonsec_conf;
   PROCESS_BEGIN();
-  //secure_init();
-  etimer_set(&timer, CLOCK_SECOND * 1);
 
+  printf("NON SECURE WORLD\n");
 
+  static int non_secure_variable2 = 82;
 
+  printf("non_secure_variable value: %d, memory address: %p \n", non_secure_variable, &non_secure_variable);
+  printf("non_secure_variable value: %d, memory address: %p \n", non_secure_variable1, &non_secure_variable1);
+  printf("non_secure_variable value: %d, memory address: %p \n", non_secure_variable2, &non_secure_variable2);
+  printf("non_secure_variable value: %d, memory address: %p \n", non_secure_variable3, &non_secure_variable3);
+  printf("non_secure_variable value: %d, memory address: %p \n", non_secure_variable4, &non_secure_variable4);
 
+  while (1){
+    // PROCESS_YIELD();
 
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-	printf("NON SECURE\n");
-
-    //SCB->VTOR = VECT_TAB_BASE_ADDRESS | VECT_TAB_OFFSET;
-
-//    printf("From non-secure world, calling non-secure callable call %d \n",secure_funcX());
-if(1){
-    printf("From non-secure world, calling non-secure callable %d \n",secure_func_entryX(200));
-}
-    //	printf("From non-secure world, calling secure function %d \n", secure_func_non_objX());
-
-
-  while(1) {
-
-    PROCESS_YIELD();
-
-
-    printf("Hello, world %d\n", i);
-    i++;
-    /* Wait for the periodic timer to expire and then restart the timer. */
-    //PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-    //etimer_reset(&timer);
+    printf("Hello, world non secure\n");
   }
+
+  /* Wait for the periodic timer to expire and then restart the timer. */
+  // PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+  // etimer_reset(&timer);
 
   PROCESS_END();
 }
