63a77d788da4d26fb5d634a4e88eec67a5d78e04
megman <johnkanwar@hotmail.com>
2021-11-26 13:00:21 +0100

    Add two different examples, SPM like configuration and TF-M like configuration

--- examples/trust_zone/secure_zone/example_s.c
+++ examples/trust_zone/secure_zone/example_s.c
@@ -9,6 +9,11 @@
 // #include "veneer_table.h"
 #include "target_cfg.h"
 
+// #include <cortex-m/tz.h>
+#include "SPM_lite.h"
+#include "dev/leds.h"
+#include "dev/etc/rgb-led/rgb-led.h"
+
 static int secure_variable = 44;
 
 #undef cmse_nsfptr_create
@@ -25,11 +30,12 @@ typedef void __attribute__((cmse_nonsecure_call)) (*tz_ns_func_ptr_t)(void);
 #define TZ_NONSECURE_FUNC_PTR_CREATE(fptr) \
   ((tz_ns_func_ptr_t)(cmse_nsfptr_create(fptr)))
 
-
-
-
 void jump_to_ns_code(void)
 {
+  spu_periph_config_uarte();
+
+  __DSB();
+  __ISB();
   /* Calls the non-secure Reset_Handler to jump to the non-secure binary */
   ns_entry();
   // tfm_core_panic();
@@ -46,15 +52,11 @@ void configure_ns_code(void)
   __TZ_set_MSP_NS(ns_msp);
 
   /* Get the address of non-secure code entry point to jump there */
-  /*Comparing the position of Reset Handler does it seem like
-    it returns an address off by one, that is the reason why
-    it is - 1U. Have tested both without and with -1U*/
-  unsigned int entry_ptr = tfm_spm_hal_get_ns_entry_point() /*- 1U*/;
+  unsigned int entry_ptr = tfm_spm_hal_get_ns_entry_point();
 
+  printf("vector_table: %p \n MSP %p \n entry point %p \n ",
+         SCB_NS->VTOR, ns_msp, entry_ptr);
 
-  printf("vector_table: %p \n MSP %p \n entry point %p \n ", 
-        SCB_NS->VTOR, ns_msp, entry_ptr);
-  
   // ns_entry = (nsfptr_t)(*((uint32_t *)((tfm_spm_hal_get_ns_VTOR()) + 4U)));
   // TZ_NONSECURE_FUNC_PTR_DECLARE(ns_entry);
   // ns_entry = TZ_NONSECURE_FUNC_PTR_CREATE(entry_ptr);
@@ -114,6 +116,8 @@ static void boot_image(void)
   entry_point();
 }
 /*END Sparrow test*/
+#define cmse_is_nsfptr(p) (!((__INTPTR_TYPE__)(p)&1))
+
 
 /*---------------------------------------------------------------------------*/
 PROCESS(example_s_process, "Example s process");
@@ -126,22 +130,99 @@ PROCESS_THREAD(example_s_process, ev, data)
 
   printf("SECURE WORLD\n");
   printf("Secure_variable value: %d, memory address: %p \n", secure_variable, &secure_variable);
+  leds_single_on(0);
 
-  core_init();
+
+  
+  /*Zephyr SPM example*/
+  if (SPM_EXAMPLE){
+    printf("Running example \n", SPM_EXAMPLE);
+
+    if (enable_fault_handlers() == TFM_PLAT_ERR_SUCCESS){
+      printf("Succsess\n");
+    }else{
+      printf("Failure\n");
+    }
+
+    /*Set flash and RAM secure, 
+      set non-secure parition non-secure for both flash and RAM
+      Set all peripherials non-secure*/
+
+    configuration_Z();
+
+    unsigned int *vtor_ns = (unsigned int)0x50000;
+
+    // vtor_ns[0] = 0x20042840;/*0x200427a0*/;
+    printf("NS IMAGE AT 0x%x \n", (unsigned int)vtor_ns);
+    printf("NS MSP AT 0x%x \n", vtor_ns[0]);
+    printf("NS reset vector AT 0x%x \n", vtor_ns[1]);
+
+    /*Things that isn't necessary but perhaps*/
+    /*Configure non-secure stack*/
+    tz_nonsecure_setup_conf_t spm_ns_conf = {
+
+        .vtor_ns = (unsigned int)vtor_ns,
+        .msp_ns = vtor_ns[0],
+        .psp_ns = 0,
+        .control_ns.npriv = 0, /* Privileged mode*/
+        .control_ns.spsel = 0  /*Use MSP in Thread mode*/
+
+    };
+
+    tz_nonsecure_state_setup(&spm_ns_conf);
+    tz_nonsecure_exception_prio_config(1);
+
+    // tz_nbanked_exception_target_state_set(0);
+    // tx_nonsecure_system_reset_req_block(IS_ENABLED)
+
+    sau_and_idau_cfg();
+
+    TZ_NONSECURE_FUNC_PTR_DECLARE(reset_ns);
+    reset_ns = TZ_NONSECURE_FUNC_PTR_CREATE(vtor_ns[1]);
+
+    if (cmse_is_nsfptr(reset_ns)){
+      printf("SPM: prepare to jump to Non-Secure image. \n");
+      spu_periph_config_uarte();
+
+      __DSB();
+      __ISB();
+      reset_ns();
+    }else{
+      printf("ERROR: WRONG POINTER TYPE\n");
+    }
+  }
 
   /*Start of configuration*/
-  sau_and_idau_cfg();
+  /* More TF-M like configuration */
+  /* Need to use the TF-M linker and startup scripts*/
+  if (TFM_EXAMPLE){
+    printf("Running example \n", TFM_EXAMPLE);
+    
+    /* Allow SPU to have precedence over (non-existing) ARMv8-M SAU */
+    sau_and_idau_cfg();
 
-  // printf("Before boot_image() \n");
-  //  boot_image();
+    /* Reset everything secure but don't lock anything */
+    /* Then configure the non-secure memory addresses in RAM and ROM secure*/
+    spu_init_cfg();
 
-  __DSB();
-  __ISB();
+    /* Print debug information */
+    print_information();
+    /*spu_periph_init_cfg() sets all perhiperals to non-secure and allocates GPIOS to non-secure domain*/
+    spu_periph_init_cfg();
 
-  printf("Jumping to non-secure code \n");
-  jump_to_ns_code();
-  printf("This shouldn't print? \n");
-  
+    /*Enable the fault handlers,
+      Enable system reset request for secure world only,
+      Setup up the non-secure pointer( non-secure region, MSP, Reset handler)*/
+    core_init();
+
+    if (cmse_is_nsfptr(ns_entry)){
+      // printf("Jumping to non-secure code \n");
+      jump_to_ns_code();
+    }else{
+      printf("ERROR: WRONG POINTER TYPE\n");
+    }
+  }
+
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
